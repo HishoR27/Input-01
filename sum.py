cd738c97119d2232cf61e6b90b342c0d9f72a04b
Gopahan Hamsavarthan <gopahanhamsavarthan@gmail.com>
2023-10-05 21:20:36 +0530

    Change the file path

--- sum.py
+++ sum.py
@@ -1,9 +1,20 @@
-file = open("D:\GOWREESAN\study\python\sum\Input-01\sum.txt", "r")
+# file = open("D:\GOWREESAN\study\python\sum\Input-01\sum.txt", "r")
+# 
+# numbers_str = file.read()
+# numbers = [
+#             int(num) 
+#             for num in numbers_str.split()
+#        ]
+# 
+# print(sum(numbers))
 
-numbers_str = file.read()
-numbers = [
-            int(num) 
-            for num in numbers_str.split()
-       ]
 
-print(sum(numbers))
+file = open("sum.txt", "r")
+
+num_str = file.read()
+
+number = [float(num) for num in num_str.split()]
+
+print(sum(number))
+
+#  12 45 8 24 57 10 44 11 79 51 
