27a2c38f85428a8d8ff9d669f76006660ea0b2a9
Gopahan Hamsavarthan <gopahanhamsavarthan@gmail.com>
2023-10-06 15:46:09 +0530

    Delete unwanted lines

--- sum.py
+++ sum.py
@@ -1,14 +1,3 @@
-# file = open("D:\GOWREESAN\study\python\sum\Input-01\sum.txt", "r")
-# 
-# numbers_str = file.read()
-# numbers = [
-#             int(num) 
-#             for num in numbers_str.split()
-#        ]
-# 
-# print(sum(numbers))
-
-
 file = open("sum.txt", "r")
 
 num_str = file.read()
